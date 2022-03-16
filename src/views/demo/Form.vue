8798014d5cafeabe0c64bdf5e7e3d122dfe6105d
tolking <qw13131wang@gmail.com>
2022-03-16 21:32:29 +0800

    feat: 增加表单

--- src/views/demo/Form.vue
+++ src/views/demo/Form.vue
@@ -1,7 +1,56 @@
 <template>
-  <p>{{ a }}</p>
+  <pro-form
+    v-model="form"
+    :columns="columns"
+    label-width="100px"
+    @submit="submit"
+  />
 </template>
 
 <script setup lang="ts">
-const a = ref('form')
+import { useForm } from '../../composables/index'
+import { Api } from '../../utils/index'
+import RichEditorVue from '../../components/RichEditor.vue'
+import type { ArticleForm } from '../../types/article'
+
+const { form, submit } = useForm<ArticleForm>({ url: Api.article })
+const columns = defineFormColumns<ArticleForm>([
+  {
+    label: '标题',
+    prop: 'title',
+    component: markRaw(ElInput),
+    rules: { required: true, message: '请输入标题', trigger: 'blur' },
+    props: {
+      clearable: true,
+      placeholder: '请输入标题',
+    },
+  },
+  {
+    label: '作者',
+    prop: 'author',
+    component: markRaw(ElInput),
+    rules: { required: true, message: '请输入作者', trigger: 'blur' },
+    props: {
+      clearable: true,
+      placeholder: '请输入作者',
+    },
+  },
+  {
+    label: '标签',
+    prop: 'tag',
+    component: markRaw(ProInputTag),
+    rules: { required: true, message: '请输入标签', trigger: 'blur' },
+    props: {
+      clearable: true,
+      trigger: 'enter',
+      placeholder: '请输入标签',
+    },
+  },
+  {
+    label: '内容',
+    prop: 'content',
+    component: markRaw(RichEditorVue),
+    rules: { required: true, message: '请输入内容', trigger: 'blur' },
+  },
+])
 </script>
