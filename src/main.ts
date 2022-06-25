5d789722454036d9dfa43b8cd8e3637a9a8f172a
tolking <qw13131wang@gmail.com>
2022-06-25 19:37:40 +0800

    feat: add asyncRouter demo

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import ElementPro from 'element-pro-components'
 import App from './App.vue'
-import router from './router'
+// NOTE: 普通路由 demo
+import router from './router/index'
+// NOTE: 异步路由 demo
+// import router from './router/asyncRouter'
 import 'element-plus/theme-chalk/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'element-pro-components/lib/styles/index.css'
