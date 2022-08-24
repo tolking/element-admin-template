3b51770bb56d6a4b529f1fae0ac526f669c03366
extittivns03 <Changhui.Zhang.EXT@desaysv.com>
2022-08-24 17:18:40 +0800

    :bug: fix refresh page 404 (#30)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { markRaw } from 'vue'
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import { House } from '@element-plus/icons-vue'
 import { useGlobalState } from '../composables/index'
 import { AllowList } from '../utils/index'
@@ -61,7 +61,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
